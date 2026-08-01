13abd7bc1f4cd6c9accd7d91d6e9f5807dae99a5
Dr. Eduardo Velasco <71657645+eduardovelascosanchez@users.noreply.github.com>
2026-08-01 13:59:10 -0600

    Agregar caché PWA NRP Remoto V2.0

---
+++ sw-remote.js
@@ -0,0 +1,31 @@
+const CACHE="nrp-escenarios-remoto-v2-0";
+const ASSETS=[
+  "./remote.html",
+  "./remote.css",
+  "./remote-app.js",
+  "./manifest-remote.webmanifest",
+  "./icon-remote.svg",
+  "./index.html"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).catch(()=>caches.match("./remote.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  })));
+});
